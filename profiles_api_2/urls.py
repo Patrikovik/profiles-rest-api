c77114ad8a8f358ee3b7b48a12376e566b71aeb0
Patrikovik <guilherme.rbhm@gmail.com>
2020-10-09 22:35:18 -0300

    API views were inicializated. URLS in profiles project 2 and profiles_api_2 were started

---
+++ profiles_api_2/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from profiles_api_2 import views
+
+
+urlpatterns = [
+    path('hello-view/',views.HelloApiView.as_views()),
+]
--- profiles_api_2/views.py
+++ profiles_api_2/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Teste API view"""
+    def get(self, request, format = None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get,post,patch,put,delete)',
+            'Is similar to a tradicional Django View',
+            'Gives you most control over you applicantion logic',
+            'Is mapped manually to URLS',
+        ]
+
+        return Response({'message':'Hello!','an_apiview': an_apiview})
